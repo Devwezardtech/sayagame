a4c51c7a45298779f48ab193418373d2f0991cf7
ejemarmaloloy-on <ejemarmaloloyon007@gmail.com>
2025-01-21 22:34:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+      const nextButton = document.getElementById('nextButton');
+
+      window.onload = function() {
+         alert("Notice: This trick works exclusively on TOSS COIN.");
+      }
+
+      window.onload = function() {
+         alert("Please ensure to take a screenshot or make a note before you begin.");
+      }
+      
